6b148b9a385ab861cb63569ddd910e4aa1917d49
Grzegorz <tarcz.g@gmail.com>
2023-04-13 20:34:02 +0200

    reload images2

--- resources/js/main.js
+++ resources/js/main.js
@@ -22,7 +22,7 @@ const context ={
             name:'Data Science Portfolio ',
             technology: 'HTML/CSS/JS',
             description: 'Strona przedstawiająca wyniki analiz z m.in. Python + Dash + Pandas',
-            image:'./resources/images/DSportfolio.JPG',
+            image:'./resources/images/dsport.JPG',
             url:'https://tarczg.github.io/DsPortfolio/',
             live:'https://tarczg.github.io/DsPortfolio/'
         },
@@ -37,7 +37,7 @@ const context ={
             name:'Analiza wpływu biedy na długość życia',
             technology: 'PYTHON, PANDAS, DASH',
             description: 'Analiza wpływu zamożności krajów na długość życia mieszkańców na przestrzeni lat i kontynentów',
-            image:'./resources/images/pandas.JPG',
+            image:'./resources/images/dsport.JPG',
             url:'https://github.com/TarczG/dashApp'
         },
         {
